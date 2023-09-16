955cc3bac87654a674a1633db55b761d70fe97c8
Meriem1551 <meriem200409@gmail.com>
2023-09-16 15:38:45 +0200

    upload project

--- src/main/java/org/math/Main.java
+++ src/main/java/org/math/Main.java
@@ -1,7 +1,20 @@
 package org.math;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);//reading from terminal
+        System.out.print("Age: ");
+        byte age = scanner.nextByte(); //parse only byte numbers
+        System.out.println("You are "+ age);
+
+        System.out.print("Name: ");
+        String name = scanner.next();// if I enter Meriem BOUSSAID it returns Meriem that's why we use nextline();
+        System.out.println("You are " + name);
+
+        System.out.print("Full Name: ");
+        String fullName = scanner.nextLine();//returns the whole entered line it also takes spaces
+        System.out.print("Your full name is: " + fullName);
     }
 }
